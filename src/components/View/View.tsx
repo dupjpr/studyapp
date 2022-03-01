4b5c503686b37c9af9c8cdf00d7f306e4dee6141
dupjpr <dupjpr@gmail.com>
2022-03-01 16:17:09 -0500

    added the render list items

--- src/components/View/View.tsx
+++ src/components/View/View.tsx
@@ -2,9 +2,23 @@ import { useAppSelector } from "../../redux/hooks";
 
 const View: React.FC = () => {
   const data = useAppSelector((state) => state.users.list);
-  console.log(data);
 
-  return <div>this is the view component</div>;
+  return (
+    <div>
+      {data.map((item) => (
+        <div key={item.id}>
+          <h3>{item.name}</h3>
+          <img src={item.img} alt="avatar" />
+          <div>
+            <strong>Age:</strong> {item.age}
+          </div>
+          <div>
+            <strong>Company: {item.company}</strong>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default View;
